b44ed037ee32de6fe3b397386cfd56cf7834a59f
Evans Thesee <EThesee@phtcorp.com>
2017-01-03 10:41:07 -0500

    a little cleaning up of comments

--- public/app/router.js
+++ public/app/router.js
@@ -27,7 +27,6 @@ define([
           initialize: function(){
           	this.flashView = new FlashMessageView();
           	this.clocks = new Clocks();
-          	//this.mainView = new ChooserView({collection: this.services});
             this.mainView = new MainView({collection: this.clocks});
           	
           	this.on('route:showAbout', function(){
@@ -38,21 +37,16 @@ define([
 
       	    this.on('route:showContact', function () {
       	      Utils.activeLink('Contact');
-      	      
-      	      //var mainView = new ChooserView({collection: services, router: this});
       	      this.mainView.render();
       	    });
       	    
-            this.on('route:showClock', function(id){
-              //console.log("showProtocol called with id:" + id);
-              Utils.activeLink('Timeline');
-              //find the protocol
-              // var pr = this.protocols.get(id);
-              // console.log("got back:", pr);
-              var timeline = new TimelineView({model: this.protocols.get(id)});
-              timeline.render();
-              timeline.afterRender();
-            });
+            // this.on('route:showClock', function(id){
+            //   //console.log("showProtocol called with id:" + id);
+            //   Utils.activeLink('Timeline');
+            //   var timeline = new TimelineView({model: this.protocols.get(id)});
+            //   timeline.render();
+            //   timeline.afterRender();
+            // });
 
       	    this.on('route:defaultHome',function(){
       	      $("#Wcontainer").empty();
